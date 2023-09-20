69502019a41ebfd33218c13af19dd43b2c64e00a
RLumSK <sebastian.kreutzer@uni-heidelberg.de>
2023-09-20 13:56:51 +0200

    Initial commit

---
+++ global.R
@@ -0,0 +1,187 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Title:   Lyoluminescence PMT App 
+## Author:  Sebastian Kreutzer, Institute of Geography, Ruprecht-Karl-University of Heidelberg (DE)
+## Contact: sebastian.kreutzer@uni-heidelberg.de
+## Date:    Wed Aug  9 10:39:24 2023
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# State required R packages -----------------------------------------------
+require(shiny)
+require(shinyjs)
+require(serial)
+require(markdown)
+require(knitr)
+require(promises)
+require(future)
+plan(multisession)
+
+# Define helper functions -------------------------------------------------
+#' Initiate the connection
+#' 
+#' @export
+init_con <- function(port) {
+  con <- serial::serialConnection(
+    name = "Lyoluminescence_ASCII",
+    port = port,
+    mode = "9600,n,8,1",
+    newline = 1,
+    translation = "cr",
+    handshake = "none") 
+  
+  ## check text
+  text_ <- paste0("Connecting to ", con$port)
+  
+  ## only open if not opened 
+  if (!serial::isOpen(con)) {
+    open(con)
+    
+    ## write response 
+    if (serial::isOpen(con)) 
+      cli::cli_alert_success(text_)
+    else
+      cli::cli_alert_danger(text_)
+    
+    ## communicate with connection ...otherwise it will not work
+    ## the "D" (default PMT), appears to be the safest character
+    write.serialConnection(con, "D")
+    read.serialConnection(con)
+    Sys.sleep(1)
+    
+  } else {
+    cli::cli_alert_info(paste0(text_, " ... already connected!"))
+    
+  }
+  
+  ## set mode
+  con$usr_status <- "connected"
+  con$usr_mode <- "initialised"
+  
+  return(con)
+}
+
+
+#' Define function for general command sending and return value reading
+#' 
+#' @export
+send_cmd <- function(con, cmd) {
+  ## treat characters ---------------
+  cmd <- toupper(cmd)
+  
+  ## extract characters and numbers
+  cmd_char <- regmatches(x = cmd, regexec("[A-Z]", text = cmd))[[1]]
+  cmd_num <- regmatches(x = cmd, regexec("[0-9]+", text = cmd))[[1]]
+  
+  ## check allowed commands
+  if (!cmd_char %in% c("P", "R", "D", "S", "C")) {
+    stop("[send_cmd()] Unsupported command! Supported are: 
+         \nP\t->\t set the integration interval in 10 ms increments (1 to 100)
+         R\t->\t set sequence of readings (1 to 255)
+         D\t->\t reset HV to default
+         S\t->\t start reading sequence
+         C\t->\t start/stop continous reading 
+         ", call. = FALSE)
+  }
+  
+  ## translate numbers into ASCII characters
+  if (length(cmd_num) > 0) {
+    ## TODO ... do better split with > 127
+    cmd_num <- rawToChar(as.raw(cmd_num), multiple = TRUE)
+    cmd_char <- paste0(cmd_char, cmd_num)
+  }
+  
+  ## construct call
+  cmd <- cmd_char
+  
+  ## write commands ------------------
+  ## write command, but we have to pay attention, if the command is OTHER
+  ## than C, we first have to make sure that we stop reading 
+  
+  ## make sure were are in a safe mode
+  
+  ## flush connection
+  con$translation <- "binary"
+  serial::read.serialConnection(con)
+  flush(con)
+  
+  ## set connection to ASCII mode
+  con$translation <- "cr"
+  
+  ## write command
+  serial::write.serialConnection(con, cmd)
+  Sys.sleep(0.5)
+  
+  ## set connection to binary mode
+  con$translation <- "binary"
+  
+  ## read response
+  resp <- serial::read.serialConnection(con) |> 
+    readBin(what = "character", size = 2, n = 1) 
+  
+  ## make sure that linebreaks are shown cmd
+  cmd <- gsub('\n', '\\n', cmd, fixed = TRUE)
+  
+  if (resp == "VA" || (cmd == "C" && resp == "")) {
+    cli::cli_alert_success(paste0("sent: <", as.character(cmd), "> | received: ", resp), wrap = TRUE)
+    
+  } else {
+    cli::cli_alert_danger(paste0("sent: <", as.character(cmd), "> | received: ", resp), wrap = TRUE)
+    
+  }
+  
+  ## set mode
+  con$mode <- cmd
+  
+  return(list(con = con, resp = resp))
+}
+
+#'
+#'
+#'@export
+read_count_data <- function(con, freq = 10) {
+  serial::nBytesInQueue(con)
+  con$translation <- "binary"
+  
+  ## read buffer (and throw it away)
+  serial::read.serialConnection(con)
+  
+  ## set list
+  m <- list()
+  
+  # read stuff
+  cnt <- 1
+  while (serial::nBytesInQueue(con)[1] <= 2048 && cnt <= freq) {
+    in_queue <- serial::nBytesInQueue(con)[1]
+    if (in_queue >= 4) {
+      bytes <- serial::read.serialConnection(con = con, n = in_queue - (in_queue %% 4))
+      n_points <- length(bytes) / 4
+      
+      ## write into vector
+      ## with the condition we make sure that nothing odd happens
+      if(length(bytes) >= 4) {
+        ## read information
+        tmp <- readBin(
+          con = bytes,
+          what = "integer",
+          size = 4,
+          n = n_points,
+          endian = "big",
+          signed = TRUE)
+        
+        ## make sure that we catch overexposure 
+        ## and set it NA
+        tmp[tmp < 0] <- NA_integer_
+        
+        ## write to matrix
+        m[cnt:(cnt - 1 + n_points)] <- tmp
+      }
+      
+      ## update counter
+      cnt <- cnt + n_points
+    }
+  }
+  
+  ## remove NA
+  return(unlist(m))
+}
+
+
---
+++ ui.R
@@ -0,0 +1,105 @@
+shinyUI(
+  navbarPage(
+    title = HTML(paste0("Lyoluminescence PMT App")),
+    windowTitle = "Lyoluminescence PMT App",
+    footer = HTML(
+      "<hr>
+            <div align = 'center'><small>This software comes WITHOUT ANY WARRANTY.</small>
+            </div>"),
+    # PANEL - SETTINGS --------------------------------------------------------
+    tabPanel("Measurement",
+      sidebarLayout(
+        sidebarPanel(
+          fluidRow(
+            column(
+              width = 12, 
+              selectInput(
+                inputId = "port", 
+                label = HTML("Select Port"), 
+                choices = serial::listPorts())
+            )),
+          fluidRow(
+            column(
+              align = "center",
+              width = 12, 
+              br(),
+              actionButton(
+                inputId = "init_start", 
+                label = HTML("Initialse")),
+              actionButton(
+                inputId = "init_stop", 
+                label = HTML("Disconnect"))
+            )),
+          hr(),
+         fluidRow(
+           column(
+             width = 12
+             ,
+             align = "center",
+             numericInput(
+               inputId =  "PMT_P",
+               label = paste0("Channel time [x 10 ms]"),
+               value = 10,
+               min = 10,
+               max = 99,
+               step = 1,
+               width = "200px"
+             ), 
+             textOutput("meas_interval"),
+             br()
+           )
+         ),
+         fluidRow(
+           column(
+             width = 12,
+             align = "center", 
+             actionButton(
+               inputId = "start", 
+               class = "btn-success",
+               label = HTML("Start reading")),
+             actionButton(
+               inputId = "stop", 
+               class = "btn-warning",
+               label = HTML("Stop reading")))
+          ),
+         hr(),
+         fluidRow(
+          column(
+            width = 12,
+            verbatimTextOutput("con_status")
+          )
+         ),
+         fluidRow(
+           column(
+             width = 12,
+             align = "center",
+            h4(textOutput("overexposure"), style = 'color:red')  
+         )
+        ),
+        ),#end sidebarPanel
+        mainPanel(
+             fluidRow(
+             div(downloadButton("download_data", "Download CSV"), align = "right"),
+               plotOutput("plot")),
+            # fluidRow(
+            #   ), 
+            icon = icon("cog", lib = "glyphicon")))
+        ), #end sidePanel
+    # PANEL - News -------------------------------------------------------------
+    tabPanel("News",
+             fluidRow(
+               column(10, offset = 1,
+                      uiOutput('news')
+               )
+             ),icon = icon("list-alt", lib = "glyphicon")
+    ),# end news
+    # PANEL - About ------------------------------------------------------------------------------
+    tabPanel("About",
+             fluidRow(
+               column(10, offset = 1,
+                      uiOutput('about')
+               )
+             ),icon = icon("info-sign", lib = "glyphicon")
+    )# end about
+  )##navbarPage
+)##EOF
